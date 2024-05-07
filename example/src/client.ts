1f6a33d89e94116bbbc29d64d382cae972875d88
Marco Link <marco.link@contentful.com>
2024-05-07 17:42:36 +0200

    feat: add example gallery

--- example/src/client.ts
+++ example/src/client.ts
@@ -1,12 +1,13 @@
 import {createClient} from "contentful";
-import {TypeBlurhashImageSkeleton} from "./types.ts";
+import {TypeBlurhashImageWithoutUnresolvableLinksResponse, TypeGallerySkeleton} from "./types.ts";
 
 const client = createClient({
   space: "02antqq6yz4y",
   accessToken: "tCAITMYW1nv43wNN19yEa6tLAYxtJCRlN3TjzXhtVLM",
 })
 
-export function getBlurHashImages() {
-  return client.withoutUnresolvableLinks.getEntries<TypeBlurhashImageSkeleton>({content_type: 'blurhashImage'})
+export async function getBlurHashImages() {
+  const gallery = await client.withoutUnresolvableLinks.getEntry<TypeGallerySkeleton>('1GQojsLNbXAK5GrQ1gX0Zk')
+  return gallery.fields.images.filter((entry) => entry && entry.fields.image && entry.fields.blurhash) as TypeBlurhashImageWithoutUnresolvableLinksResponse[]
 }
 
--- example/src/types.ts
+++ example/src/types.ts
@@ -13,3 +13,16 @@ export type TypeBlurhashImageWithoutUnresolvableLinksResponse = TypeBlurhashImag
 export type TypeBlurhashImageWithAllLocalesResponse<Locales extends LocaleCode = LocaleCode> = TypeBlurhashImage<"WITH_ALL_LOCALES", Locales>;
 export type TypeBlurhashImageWithAllLocalesAndWithoutLinkResolutionResponse<Locales extends LocaleCode = LocaleCode> = TypeBlurhashImage<"WITHOUT_LINK_RESOLUTION" | "WITH_ALL_LOCALES", Locales>;
 export type TypeBlurhashImageWithAllLocalesAndWithoutUnresolvableLinksResponse<Locales extends LocaleCode = LocaleCode> = TypeBlurhashImage<"WITHOUT_UNRESOLVABLE_LINKS" | "WITH_ALL_LOCALES", Locales>;
+
+export interface TypeGalleryFields {
+  images: EntryFieldTypes.Array<EntryFieldTypes.EntryLink<TypeBlurhashImageSkeleton>>;
+  title: EntryFieldTypes.Symbol;
+}
+
+export type TypeGallerySkeleton = EntrySkeletonType<TypeGalleryFields, "gallery">;
+export type TypeGallery<Modifiers extends ChainModifiers, Locales extends LocaleCode = LocaleCode> = Entry<TypeGallerySkeleton, Modifiers, Locales>;
+export type TypeGalleryWithoutLinkResolutionResponse = TypeGallery<"WITHOUT_LINK_RESOLUTION">;
+export type TypeGalleryWithoutUnresolvableLinksResponse = TypeGallery<"WITHOUT_UNRESOLVABLE_LINKS">;
+export type TypeGalleryWithAllLocalesResponse<Locales extends LocaleCode = LocaleCode> = TypeGallery<"WITH_ALL_LOCALES", Locales>;
+export type TypeGalleryWithAllLocalesAndWithoutLinkResolutionResponse<Locales extends LocaleCode = LocaleCode> = TypeGallery<"WITHOUT_LINK_RESOLUTION" | "WITH_ALL_LOCALES", Locales>;
+export type TypeGalleryWithAllLocalesAndWithoutUnresolvableLinksResponse<Locales extends LocaleCode = LocaleCode> = TypeGallery<"WITHOUT_UNRESOLVABLE_LINKS" | "WITH_ALL_LOCALES", Locales>;
