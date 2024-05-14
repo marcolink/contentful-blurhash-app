ec3177759d98d8d3b0a545ebca498aeef0fa9ba2
Marco Link <marco.link@contentful.com>
2024-05-14 07:44:43 +0200

    fix: field error message

--- src/locations/Field.tsx
+++ src/locations/Field.tsx
@@ -21,7 +21,7 @@ const Field = () => {
 
   return (
     <Note variant={'negative'} title={'Not configured'}>
-      This app requires a field of type <strong>Link</strong> to an <strong>Asset</strong> with the id <code>image</code> to be
+      This app requires a field of type <strong>Link</strong> to an <strong>Asset</strong> with the id <code>{parameters.sourceImageFieldId}</code> to be
       present on the <a href={contentTypeLink} target={'_blank'}>content type</a>.
       Ideally, the field is restricted to only allow image assets.
     </Note>
