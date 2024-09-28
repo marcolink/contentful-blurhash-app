b1799324bbadc1a69786482a836e67398601e30b
Marco Link <marco.link@contentful.com>
2024-09-28 11:31:55 +0200

    test: add ci test setep

--- test/mocks/mockCma.ts
+++ test/mocks/mockCma.ts
@@ -1,3 +1,25 @@
-const mockCma: any = {};
+const mockCma: any = {
+  editorInterface: {
+    get: async (params: any) => {
+      return {
+        controls: []
+      };
+    },
+    update: async (params: any, editorInterface: any) => {
+      return editorInterface;
+    }
+  },
+  contentType: {
+    createWithId: async (params: any, contentType: any) => {
+      return {...contentType, sys: {id: params.contentTypeId}};
+    },
+    publish: async (params: any, contentType: any) => {
+      return contentType;
+    },
+    get: async (params: any) => {
+      return null;
+    }
+  }
+};
 
 export { mockCma };
